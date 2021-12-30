88ab50b39feb17b25a1a267f3f143e28cec9df57
Matthias Kuhn <mkuhn1@uni-muenster.de>
2021-12-30 19:38:05 +0100

    added status display class

---
+++ main/StatusDisplay.h
@@ -0,0 +1,41 @@
+#ifndef StatusDisplay_h
+#define StatusDisplay_h
+
+#include "Arduino.h"
+// required for "const char" and "PROGMEM"
+#include <avr/pgmspace.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+class StatusDisplay {
+  public:
+  StatusDisplay();
+  void setup();
+  void updateOnLoop(
+    unsigned long currentTime, unsigned long remainingBreak, 
+    float temperature, float humidity, float moisture, 
+    bool isWatering);
+
+  private:
+  void showDHTScreen(float t, float h);
+  void showMoistureScreen(unsigned long time, float moisture);
+  void updateSeconds(unsigned long time);
+  void showWateringScreen();
+
+  const char* timeEnding = "";
+  bool onUpdateLoopIsCalled = false;
+  int lastTime = 0;
+  bool updateTime = true;
+
+  Adafruit_SSD1306* display;
+  int activeDisplay;
+
+
+
+};
+
+#endif
